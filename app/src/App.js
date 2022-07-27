cb70a1670659d4a7ee47cd93cf0fe75917a358a8
Vinicius Albano <vinialbano@gmail.com>
2022-07-26 20:06:59 -0700

    feat: add basic navigation and countdown

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,61 @@
 import React from "react";
+import { intervalToDuration } from "date-fns";
+import { Routes, Route, Link } from "react-router-dom";
+
+export function LaunchesList(props) {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <h1 className="mt-2 text-4xl font-extrabold text-black/75 tracking-tight sm:text-5xl mb-8">
+        Next Launches
+      </h1>
+      <div className="flex flex-col px-4 py-5 bg-white/40 shadow rounded-lg overflow-hidden sm:p-6">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+                  >
+                    Mission
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Date (UTC)
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Launchpad
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {props.launches.map((launch) => (
+                  <tr key={launch.mission}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 md:pl-0">
+                      {launch.mission}
+                    </td>
+                    <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
+                      {launch.date.toUTCString()}
+                    </td>
+                    <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
+                      {launch.launchpad}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export function LaunchDate(props) {
   return (
@@ -22,16 +79,63 @@ export function LaunchDate(props) {
   );
 }
 
+export class CurrentLaunch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.name,
+      date: props.date,
+      duration: intervalToDuration({
+        start: new Date(),
+        end: props.date,
+      }),
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(
+      () =>
+        this.setState({
+          duration: intervalToDuration({
+            start: new Date(),
+            end: this.state.date,
+          }),
+        }),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <>
+        <h1 className="mt-2 text-4xl font-extrabold text-black/75 tracking-tight sm:text-5xl mb-8">
+          Upcoming: {this.state.name}
+        </h1>
+        <LaunchDate
+          dates={[
+            { name: "Days", stat: this.state.duration.days },
+            { name: "Hours", stat: this.state.duration.hours },
+            { name: "Minutes", stat: this.state.duration.minutes },
+            { name: "Seconds", stat: this.state.duration.seconds },
+          ]}
+        />
+      </>
+    );
+  }
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      launchName: "SXM-8",
-      launchDates: [
-        { name: "Days", stat: "2" },
-        { name: "Hours", stat: "7" },
-        { name: "Minutes", stat: "59" },
-        { name: "Seconds", stat: "31" },
+      nextLaunches: [
+        {
+          mission: "SAOCOM 1B & Smallsat Rideshare 1",
+          date: new Date("2023-07-01T00:00:00.000Z"),
+          launchpad: "CCAFS SLC-40",
+        },
       ],
     };
   }
@@ -46,10 +150,25 @@ export default class App extends React.Component {
         }}
       >
         <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-          <h1 className="mt-2 text-4xl font-extrabold text-black/75 tracking-tight sm:text-5xl mb-8">
-            Upcoming: {this.state.launchName}
-          </h1>
-          <LaunchDate dates={this.state.launchDates} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <LaunchesList launches={this.state.nextLaunches}></LaunchesList>
+              }
+            />
+            {/* {this.state.nextLaunches.map((launch) => ( */}
+            <Route
+              path={`/current`}
+              element={
+                <CurrentLaunch
+                  name={this.state.nextLaunches[0].mission}
+                  date={this.state.nextLaunches[0].date}
+                ></CurrentLaunch>
+              }
+            />
+            {/* ))} */}
+          </Routes>
         </div>
       </main>
     );
